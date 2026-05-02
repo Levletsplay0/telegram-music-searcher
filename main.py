26b3c0c7c66fb8e78b5b36ce3e7553fd91cdc5ba
levletsplay0 <levbro14@gmail.com>
2026-05-02 14:22:00 +0700

    fixing minor bugs and improving the bot

--- main.py
+++ main.py
@@ -1,34 +1,57 @@
 import telebot
 from music import get_track_info, download_audio
 import os
+import logging
+
+
+logging.basicConfig(level=logging.INFO)
 
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-print(BOT_TOKEN)
+
+if not BOT_TOKEN:
+    logging.info("Переменная окружения BOT_TOKEN не установлена")
+    raise ValueError("Переменная окружения BOT_TOKEN не установлена")
 
 bot = telebot.TeleBot(token=BOT_TOKEN)
+logging.info("Бот успешно запущен")
 
 @bot.message_handler(commands=["start"])
 def welcome(message):
-    bot.send_message(message.chat.id, f"Привет, {message.from_user.first_name}! Напишите название песни или исполнителя и я вам найду её для вас!")
+    bot.send_message(message.chat.id, f"Привет, {message.from_user.first_name}! Напишите название песни или исполнителя, и я найду её для вас!")
 
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
-    track_info = get_track_info(message.text)
+    try:
+        track_info = get_track_info(message.text)
+    except Exception as e:
+        logging.error(f"Ошибка поиска информации о треке: {e}")
+        bot.send_message(message.chat.id, "Ошибка: не удалось найти трек.")
+
     if track_info:
-        bot.send_photo(message.chat.id, track_info["cover_url"], caption=f"🎵 Название: {track_info['title']}\n🎤 Исполнитель: {track_info['artists']}\n💿 Альбом: {track_info['album']}\n⏱ Длительность: {track_info['duration']}\nVideo_id: {track_info['video_id']}")
+        bot.send_photo(message.chat.id, track_info["cover_url"], caption=f"🎵 Название: {track_info['title']}\n🎤 Исполнитель: {track_info['artists']}\n💿 Альбом: {track_info['album']}\n⏱ Длительность: {track_info['duration']}\nid видео: {track_info['video_id']}")
         status_message = bot.send_message(message.chat.id, "Скачивание на сервере, подождите немного...")
 
-        path = download_audio(track_info["video_id"])
-        bot.delete_message(status_message.chat.id, status_message.message_id)
+        try:
+            path = download_audio(track_info["video_id"])
+            bot.delete_message(status_message.chat.id, status_message.message_id)
+        except Exception as e:
+            logging.error(f"Ошибка при скачивании аудио: {e}")
+            bot.delete_message(status_message.chat.id, status_message.message_id)
+            bot.send_message(message.chat.id, "Ошибка: не удалось скачать аудиофайл.")
         
-        with open(path, "rb") as audio_file:
-            bot.send_audio(
-                message.chat.id,
-                audio_file,
-                title=track_info["title"],
-                performer=track_info["artists"],
-                thumbnail=track_info["cover_url"]
-            )
-        
+        if path and os.path.exists(path):
+            with open(path, "rb") as audio_file:
+                bot.send_audio(
+                    message.chat.id,
+                    audio_file,
+                    title=track_info["title"],
+                    performer=track_info["artists"],
+                    thumbnail=track_info["cover_url"]
+                )
+        else:
+            bot.send_message(message.chat.id, "Ошибка: не удалось скачать аудиофайл.")
+    else:
+        bot.send_message(message.chat.id, "Не удалось найти трек по вашему запросу. Попробуйте еще раз с другим названием.")
+
 bot.infinity_polling()
--- music.py
+++ music.py
@@ -1,66 +1,77 @@
 from ytmusicapi import YTMusic
 import yt_dlp
 from pathlib import Path
-import json
+import logging
 
+logging.basicConfig(level=logging.INFO)
 
 ytmusic = YTMusic()
+logging.info("YTMusic API и yt_dlp успешно инициализированы")
 
 def get_track_info(query: str):
     if not query:
+        logging.warning("Пустой запрос для поиска трека")
         return None
     
-    results = ytmusic.search(query, filter="songs", limit=1)
+    try:
+        results = ytmusic.search(query, filter="songs", limit=1)
 
-    if not results:
+        if not results:
+            logging.warning("Трек не найден")
+            return None
+            
+        track = results[0]
+
+        title = track.get("title", "Unknown")
+        artists = ", ".join(a.get("name", "Unknown") for a in track.get("artists", []))
+        album = track.get("album", {}).get("name", "Unknown")
+        duration = track.get("duration", "Unknown")
+        video_id = track.get("videoId")
+
+        thumbnails = track.get("thumbnails", [])
+        cover_url = thumbnails[-1]["url"] if thumbnails else None
+
+
+
+        return {
+            "title": title,
+            "artists": artists,
+            "album": album,
+            "duration": duration,
+            "cover_url": cover_url,
+            "video_id": video_id
+        }
+    except Exception as e:
+        logging.error(f"Error fetching track info: {e}")
         return None
-        
-    track = results[0]
-
-    title = track.get("title", "Unknown")
-    artists = ", ".join(a.get("name", "Unknown") for a in track.get("artists", []))
-    album = track.get("album", {}).get("name", "Unknown")
-    duration = track.get("duration", "Unknown")
-    video_id = track.get("videoId")
-
-    thumbnails = track.get("thumbnails", [])
-    cover_url = thumbnails[-1]["url"] if thumbnails else None
-
-
-
-    return {
-        "title": title,
-        "artists": artists,
-        "album": album,
-        "duration": duration,
-        "cover_url": cover_url,
-        "video_id": video_id
-    }
 
 
 
 def download_audio(video_id: str, output_dir: str = "./downloads"):
     file = Path(f"{output_dir}/{video_id}.mp3")
     if file.is_file():
+        logging.info(f"Файл уже существует: {file}")
         return f"{output_dir}/{video_id}.mp3"
 
     url = f"https://music.youtube.com/watch?v={video_id}"
-    
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'postprocessors': [{                 
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
-        }],
-        'outtmpl': f'{output_dir}/{video_id}.%(ext)s',
-        'quiet': True,
-        'no_warnings': True,
-    }
+    try:
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'postprocessors': [{                 
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+            'outtmpl': f'{output_dir}/{video_id}.%(ext)s',
+            'quiet': True,
+            'no_warnings': True,
+        }
 
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
-        print(f"✅ Сохранено в {output_dir}/{video_id}.mp3")
-        with open("tracks.json", "w+") as f:
-            json.dump({"video_id": video_id}, f, indent=4)
-        return f"{output_dir}/{video_id}.mp3"
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+            logging.info(f"✅ Сохранено в {output_dir}/{video_id}.mp3")
+            
+            return f"{output_dir}/{video_id}.mp3"
+    except Exception as e:
+        logging.error(f"Error downloading audio: {e}")
+        return None
